b43a360276f7f9aefebdb9e25a041d18c126b6f7
Vadim Engoyan <vadim@engoyan.com>
2013-02-15 12:27:52 -0800

    Add API post

--- lib/dws/src/Dws/Slender/Api/ApiClient.php
+++ lib/dws/src/Dws/Slender/Api/ApiClient.php
@@ -2,6 +2,8 @@
 
 use Zend\Http\Request as Request;
 use Zend\Http\Client as Client;
+use Zend\Http\Server\Client as ServerClient;
+// use Dws\Slender\Api\ApiException;
 
 class ApiClient {
 
@@ -36,31 +38,37 @@ class ApiClient {
     }
 
     public function post($path, array $params = array()){
+        //works
+        // $request = new Request();
+        // $request->setUri($this->getUri($path));
+        // $request->setMethod('POST');
+        // $request->getPost()->set('email', 'bar');
+
+        // $client = new Client();
+        // $client->setEncType("multipart/form-data");
+        // $response = $client->dispatch($request);
+
+        // if ($response->isSuccess()) {
+        //     //  the POST was successful
+        // }
+
         $this->request->setMethod(Request::METHOD_POST);
         $this->request->setUri($this->getUri($path));
-        $this->client->setParameterPost($params);
+        $this->request->getPost()->fromArray($params);
+        $this->client->setEncType("multipart/form-data");   
         return $this->run();
     }
 
     private function run(){
         $response = $this->client->dispatch($this->request);
-
         if($response->isSuccess()){
-
+            return json_decode($response->getBody());
         }else{
             $error = json_decode($response->getBody());
-            // throw new \Exception("Error Processing Request", 1);
-            
             throw new ApiException($error);
         }
-        var_dump($response, $response->getBody());
 
         return $response->isSuccess() ? $response->getBody() : false;
-        // if ($response) {
-        //     return Json::decode($response, true);
-        // } else {
-        //     return false;
-        // }
     }
 
     private function getUri($path, $site=null){
