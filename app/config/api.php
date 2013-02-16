636b99b614e64b46dba3804d3fca3b5058a36f44
jsamos <jsamos@gmail.com>
2013-02-16 00:32:50 -0500

    change default app key to slender

--- app/config/api.php
+++ app/config/api.php
@@ -3,6 +3,6 @@
 return array(
 	'slender' => array(
 		'host' => 'http://10.0.2.2:4003',
-		'auth' => '0d71691b9cf1958bef776c9eee6c792210cbb108' //@TODO: remove this, we will retrive auth key from User
+		'auth' => 'slender' //@TODO: remove this, we will retrive auth key from User
 	),
 );
