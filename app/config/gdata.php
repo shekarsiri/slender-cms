44acefe7ed41985a41979c8b49debfe165a403a7
andrew <ygyman@gmail.com>
2013-02-20 02:54:35 +0200

    videos upload, youtube upload

--- app/config/gdata.php
+++ app/config/gdata.php
@@ -9,14 +9,10 @@
 
 return array(
     'youtube' => array(
-        /*'api_key'          => 'AI39si7YTtfztb8dfUXuBUywVALCp3OT7qRYeNwxh-yT0xJyg3LE5maHbewh4kc5mjyxaxTKRpld_4E_gk-lRrgozavC5QL-Xg',
+        'api_key'          => 'AI39si7YTtfztb8dfUXuBUywVALCp3OT7qRYeNwxh-yT0xJyg3LE5maHbewh4kc5mjyxaxTKRpld_4E_gk-lRrgozavC5QL-Xg',
         'api_name'         => '',
         'youtube_email'    => '',
-        'youtube_pass'     => '',*/
-        'api_key'          => 'AI39si7YTtfztb8dfUXuBUywVALCp3OT7qRYeNwxh-yT0xJyg3LE5maHbewh4kc5mjyxaxTKRpld_4E_gk-lRrgozavC5QL-Xg',
-        'api_name'         => 'ygyman',
-        'youtube_email'    => 'ygyman@gmail.com',
-        'youtube_pass'     => '57 02 43'
+        'youtube_pass'     => '',
     )
 
 );
