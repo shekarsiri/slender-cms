065e781472c80ec79ae3bf034f00fb70e57f118e
andrew <ygyman@gmail.com>
2013-02-21 19:02:08 +0200

    site switcher

--- app/controllers/SitesController.php
+++ app/controllers/SitesController.php
@@ -16,4 +16,26 @@ class SitesController extends BaseController {
         }
         return Redirect::to('/');
     }
+
+    public function store()
+    {
+        $data = Input::all();
+
+        try {
+            $response = $this->api->post($this->package, $data);
+            $sites = Session::get('sites');
+            $sites[$data['slug']] = $data['title'];
+            Session::put('sites', $sites);
+        } catch (ApiException $e) {
+            $errors = new MessageBag;
+            $messages = $e->getMessages();
+            foreach ($messages[0] as $key => $value) {
+                foreach ($value as $msg) {
+                    $errors->add($key, $msg);
+                }
+            }
+            return Redirect::to($this->package."/create")->withErrors($errors);
+        }
+        return Redirect::to($this->package)->with('success', 'The data successfully saved!');
+    }
 }
