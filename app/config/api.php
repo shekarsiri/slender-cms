55922bb20149372f90c3eeac0400af24dfb0d651
jsamos <jsamos@gmail.com>
2013-02-21 12:56:48 -0500

    config for staging

--- app/config/api.php
+++ app/config/api.php
@@ -2,7 +2,16 @@
 
 return array(
 	'slender' => array(
-		'host' => 'http://10.0.2.2:4003',
-		'auth' => 'slender' //@TODO: remove this, we will retrive auth key from User
+
+        //local
+        //'host' => 'http://10.0.2.2:4003',
+        //'auth' => 'slender', //@TODO: remove this, we will retrive auth key from User
+        //'site' => 'eb'
+
+        //staging
+        'host' => 'http://api.staging.eventbidz.com',
+        'auth' => '4890e5f0-7c36-11e2-b92a-0800200c9a66', //@TODO: remove this, we will retrive auth key from User
+        'site' => 'eb'
+
 	),
 );
